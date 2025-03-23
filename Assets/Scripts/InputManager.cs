112539c05409148755955b7d315b033a4008dcab
Reyhan-Sam <reyhansam13@gmail.com>
2025-03-22 21:35:03 -0700

    refractored the player to be a prefab, reduced clutter and maintained consistancy

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour {
+    public UnityEvent<float> OnMoveHorizontal = new UnityEvent<float>();
+    public UnityEvent OnJump = new UnityEvent();
+    public UnityEvent OnDownThrust = new UnityEvent();
+
+    void Update() {
+        // Get horizontal movement using Unity's input system
+        float horizontalInput = Input.GetAxis("Horizontal");
+        OnMoveHorizontal?.Invoke(horizontalInput);
+
+        // Handle jump input
+        if (Input.GetButtonDown("Jump")) {
+            OnJump?.Invoke();
+        }
+
+        // Handle downward thrust input
+        if (Input.GetButtonDown("Vertical") && Input.GetAxis("Vertical") < 0) {
+            OnDownThrust?.Invoke();
+        }
+    }
+}
