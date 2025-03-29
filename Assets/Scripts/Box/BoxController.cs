a3d6059e927b6ad976dc10be3709417193f6e6e7
Varun Patel <rvjpatel99@gmail.com>
2025-03-28 22:18:06 -0700

    basic box animation complete

--- Assets/Scripts/Box/BoxController.cs
+++ Assets/Scripts/Box/BoxController.cs
@@ -1,23 +1,45 @@
+using UnityEditor;
 using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
     private Animator animator;
-    private int i = 1000;
+    [SerializeField] private GameObject brokenBoxPrefab;
+    [SerializeField] private GameManager gameManager;
+    private Transform transform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
      animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        i--;
-        if (i == 0)
+        if (collision.gameObject.CompareTag("Player") && !animator.GetBool("isHit"))
         {
             animator.SetBool("isHit", true);
+            Invoke(nameof(BreakBox), 0.5f);
         }
-        Debug.Log(i);
+    }
+
+    private void BreakBox()
+    {
+        if (brokenBoxPrefab != null)
+        {
+            GameObject brokenBox = Instantiate(brokenBoxPrefab, transform.localPosition, Quaternion.identity);
+
+            foreach (Transform piece in brokenBox.transform)
+            {
+                Rigidbody2D rb = piece.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.linearVelocity = new Vector2(Random.Range(-.01f, 0.1f), Random.Range(2f, 5f));
+                    rb.angularVelocity = Random.Range(-200f, 200f);
+                }
+            }
+        }
+
+        Destroy(gameObject);
     }
 }
