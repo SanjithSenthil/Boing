22ec8838090d0cc6f3c41aceb25649f48197cd2d
Reyhan-Sam <reyhansam13@gmail.com>
2025-03-30 01:15:15 -0700

    added sound effect for all scenes with audio manager prefab

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("Audio Clips")]
+    public AudioClip mainMenuMusic;
+    public AudioClip level1Music;
+    public AudioClip level2Music;
+    public AudioClip gameOverMusic;
+
+    [Header("Settings")]
+    [Range(0f, 1f)] public float musicVolume = 0.7f;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.loop = true;
+            audioSource.playOnAwake = false;
+            audioSource.volume = musicVolume;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        PlayMusicForScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayMusicForScene(scene.name);
+    }
+
+    public void PlayMusicForScene(string sceneName)
+    {
+        AudioClip clipToPlay = null;
+
+        switch (sceneName)
+        {
+            case "MainMenu":
+                clipToPlay = mainMenuMusic;
+                break;
+            case "Level 1":
+                clipToPlay = level1Music;
+                break;
+            case "Level 2":
+                clipToPlay = level2Music;
+                break;
+            case "GameOver":
+                clipToPlay = gameOverMusic;
+                break;
+        }
+
+        if (clipToPlay != null && audioSource.clip != clipToPlay)
+        {
+            audioSource.clip = clipToPlay;
+            audioSource.Play();
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = Mathf.Clamp01(volume);
+        if (audioSource != null)
+        {
+            audioSource.volume = musicVolume;
+        }
+    }
+    // private void OnEnable()
+    // {
+    //     SceneManager.sceneLoaded += OnSceneLoaded;
+    // }
+
+    // private void OnDisable()
+    // {
+    //     SceneManager.sceneLoaded -= OnSceneLoaded;
+    // }
+
+}
