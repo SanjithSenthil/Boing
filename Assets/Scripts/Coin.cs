0a1d98b30e19b824265a05899d939f94bfb1acb0
Reyhan-Sam <reyhansam13@gmail.com>
2025-03-30 01:14:06 -0700

    added sound effect for coin collection

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int scoreValue = 1;
+    public AudioClip coinCollectSound;
+    private AudioSource audioSource;
 
+
+    private void Start()
+    {
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = coinCollectSound;
+        audioSource.playOnAwake = false; // Don't play on awake
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.instance.AddScore(scoreValue);
+            AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
             Destroy(gameObject);
+
         }
     }
 
